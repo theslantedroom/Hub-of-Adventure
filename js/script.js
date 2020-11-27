5ab76e1df187d80094daada970b39f878757efbd
theslantedroom <73557008+theslantedroom@users.noreply.github.com>
2020-11-27 12:30:15 -0800

    kispyScam

--- js/script.js
+++ js/script.js
@@ -43,17 +43,77 @@ let makeSmallTicker = 1;
  			document.querySelectorAll('.kitty')[makeBigTicker].classList.add("makeBig");
 
 
-        }, 200);
+        }, 50);
 
 
         var chase = setInterval(function() {
         	makeSmallTicker += 1;
             if (makeSmallTicker==10) { makeSmallTicker = 0; makeSmallTicker = 0; };
  		document.querySelectorAll('.kitty')[makeSmallTicker].classList.remove("makeBig");
-        }, 100);
+        }, 50);
 
         // setTimeout(function() {
         //     clearInterval(logger)
         // }, 5500)
 
+const dynamicButton = document.getElementById("dynamicButton"); 
+const sample = document.getElementById("sample"); 
+const kispy = document.getElementById('kispy');
+const catMeter = document.getElementById("catMeter"); 
+const fundsStolen = document.getElementById("fundsStolen"); 
+
+kispy.addEventListener("mouseover", function(){
+    mouseOverKispy();
+});
+
+kispy.addEventListener("mouseout", function(){ 
+    mouseOutKispy();
+});
+
+kispy.addEventListener("click", function(){
+    playSample();
+});
+
+
+const mouseOverKispy = () => {
+    document.getElementById("kispy").style.backgroundImage  = "url('img/kispyPng2.png')"
+};
+
+const mouseOutKispy = () => {
+    document.getElementById("kispy").style.backgroundImage  = "url('img/kispyPng1.png')"
+};
+
+
+
+function playSample() { 
+  sample.play()
+} 
+    let funds = 124;
+
+const buttonKispyMeow = () => {
+    let frame = 1;
+        catMeter.style.display = 'block';
+    playSample()
+    dynamicButton.remove();
+    alert("I'm Kispy! meow meow meow! Are you sure you trust Me?")
+        
+        var animateKispy = setInterval(function() {
+            
+            document.getElementById("kispy").style.backgroundImage  = "url('img/kispyPng"+frame+".png')"
+            frame += 1;
+            console.log(frame)
+            if (frame==9) { 
+                frame = 1;
+                playSample();   
+                fundsStolen.style.color = "black";           
+             };
+
+              if (frame==4) { 
+                     fundsStolen.innerHTML = `Your bank account has been DRAINED of $${funds.toFixed(2)} $ WaRNING warniNG Transaction APPROVED`;     
+                     funds = funds*2.8; 
+                     fundsStolen.style.color = "red";
+             };
+            
+        }, 100);
+};
 
